5895ce0abf36b6ffdcd607c4c9c04724d90da29a
saeed_dev <saeed_dev@yahoo.com>
2020-05-02 09:40:22 +0500

    fix error in popup due to saveLink being null

--- popup.js
+++ popup.js
@@ -6,6 +6,7 @@ chrome.tabs.getSelected(null,function(tab) { // null defaults to current window
   var url = tab.url;
 
   let saveLink = document.getElementById('save-link');
-  saveLink.href = "http://www.thiscodeworks.com/newlink?url="+url+"&pagetitle="+title;
-
+  if (saveLink !== null) {
+    saveLink.href = "http://www.thiscodeworks.com/newlink?url="+url+"&pagetitle="+title;
+  } 
 });
